fd3d5e767c219bb674a8721ed715e06de28412c4
opesnoir <goodsurroundsme@outlook.com>
2024-05-23 20:34:38 +0200

    feat: launcher en readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # oop_les5_opdracht1_bedrijf_arraylist
+1. Arraylist
+2. for-each + instance of
+3. for-each + methode --> om data te printen
+
+1. // maak een arraylist
+        ArrayList<Persoon> personenArrayList = new ArrayList<>();
+        personenArrayList.add(new Werknemer("Mark", "Den Haag", afdeling[2], 10000));
+        personenArrayList.add(new Werknemer("Caroline", "Delf", afdeling[1], 4000));
+        personenArrayList.add(new Zzper("Klaas", "Diemen", afdeling[0], 50.00));
+        personenArrayList.add(new Zzper("Ronals", "Zaandam", afdeling[0], 80.00));
+        personenArrayList.add(new Zzper("Jannie", "Utrecht", afdeling[0], 60.00));
+        personenArrayList.add(new Zzper("Anne", "Zwolle", afdeling[0], 40.00));
+
+2. // gebruik for-each met instanceof om alle zzp'ers in te huren voor 320 uur
+        for (Persoon persoon : personenArrayList){ // for each loop zegt: voor elk datatype : [:=in] in de array met de naam ... doe...
+            if (persoon instanceof Zzper){ // instance of zegt parent is persoon en de instantie is zzper (is A relatie)
+                ((Zzper) persoon).huurIn(320); // type casten in, om zo bij de methode huurIn te kunnen komen
+                ((Zzper) persoon).huurIn(320);
+            }
+        }
+
+3. // gebruik for-each om de toonJaarInkomen() te printen
+        for (Persoon persoon : personenArrayList){
+            toonJaarInkomen(persoon);
+        }
+    }
--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -4,6 +4,8 @@ import model.Persoon;
 import model.Werknemer;
 import model.Zzper;
 
+import java.util.ArrayList;
+
 
 public class BedrijfLauncher {
     public static void main(String[] args) {
@@ -14,26 +16,26 @@ public class BedrijfLauncher {
         afdeling[2] = new Afdeling("Management", "Almere");
         afdeling[3] = new Afdeling("Documentatie", "Gouda");
 
-        // maak medewerkers / zzp'ers gebruik polymorfisme
-        Persoon baas = new Werknemer("Mark", "Den Haag", afdeling[2], 10000);
-        Persoon medewerker = new Werknemer("Caroline", "Delf", afdeling[1], 4000);
-        Persoon assistent = new Zzper("Klaas", "Diemen", afdeling[0], 50.00);
-        Persoon projectleider = new Zzper("Ronals", "Zaandam", afdeling[0], 80.00);
+        // maak een arraylist
+        ArrayList<Persoon> personenArrayList = new ArrayList<>();
+        personenArrayList.add(new Werknemer("Mark", "Den Haag", afdeling[2], 10000));
+        personenArrayList.add(new Werknemer("Caroline", "Delf", afdeling[1], 4000));
+        personenArrayList.add(new Zzper("Klaas", "Diemen", afdeling[0], 50.00));
+        personenArrayList.add(new Zzper("Ronals", "Zaandam", afdeling[0], 80.00));
+        personenArrayList.add(new Zzper("Jannie", "Utrecht", afdeling[0], 60.00));
+        personenArrayList.add(new Zzper("Anne", "Zwolle", afdeling[0], 40.00));
 
-        //aantal uren inhuur zzp'ers
-        ((Zzper) assistent).huurIn(160); // je moet hem eerst casten naar zzper, daar staat de methode die je wil benaderen
-        ((Zzper) projectleider).huurIn(320);
+        // gebruik for-each met instanceof om alle zzp'ers in te huren voor 320 uur
+        for (Persoon persoon : personenArrayList){ // for each loop zegt: voor elk datatype : [:=in] in de array met de naam ... doe...
+            if (persoon instanceof Zzper){ // instance of zegt parent is persoon en de instantie is zzper (is A relatie)
+                ((Zzper) persoon).huurIn(320); // type casten in, om zo bij de methode huurIn te kunnen komen
+                ((Zzper) persoon).huurIn(320);
+            }
+        }
 
-        //maak een Persoon array met 4 objecten
-        Persoon[] personen = new Persoon[4];
-        personen[0] = baas;
-        personen[1] = medewerker;
-        personen[2] = assistent;
-        personen[3] = projectleider;
-
-        // gebruik forloop om de items te printen
-        for (int i = 0; i < personen.length; i++) {
-            toonJaarInkomen(personen[i]);
+        // gebruik for-each om de toonJaarInkomen() te printen
+        for (Persoon persoon : personenArrayList){
+            toonJaarInkomen(persoon);
         }
     }
 
